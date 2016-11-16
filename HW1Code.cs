1aef08b3b1d2be9b25c6e3c3fa12243d2f84eb25
Jsalaz <jorgesalazar.cs@gmail.com>
2016-11-15 23:28:26 -0500

    First C# program
    Simple programing questions aong with learning C# and NUnit

---
+++ HW1Code.cs
@@ -0,0 +1,520 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HW1Test
+{
+	public class HW1Code
+	{
+		// 1. returns the largest of two values
+		public static int TwoMax(int arg1, int arg2)
+		{
+			int retVal = 0;
+			if (arg1 == arg2)
+			{
+				Console.WriteLine("Two Values are Equal");
+				retVal = arg1;
+			}
+			if (arg1 > arg2)
+			{
+				retVal = arg1;
+			}
+			else if (arg2 > arg1)
+			{
+				retVal = arg2;
+			}
+
+			return retVal;
+		}
+
+
+		// 2. returns the largest of three values
+		public static int ThreeMax(int arg1, int arg2, int arg3)
+		{
+			int retVal;
+			if ((arg1 == arg2) && (arg2 == arg3))
+			{
+				Console.WriteLine("All Values are Equal");
+				retVal = arg1;
+			}
+			else {
+				retVal = TwoMax(arg1, arg2);
+				retVal = TwoMax(retVal, arg3);
+			}
+			return retVal;
+		}
+
+		// 3. Returns true if char is a vowel
+		public static bool StartsWithVowel(char let1)
+		{
+			
+			switch (let1)
+			{
+				case 'a':
+				case 'A':
+				case 'e':
+				case 'E':
+				case 'i':
+				case 'I':
+				case 'o':
+				case 'O':
+				case 'u':
+				case 'U':
+					return true;
+				//break;
+				default:
+					return false;
+					//break;
+			}
+		}
+
+		// 4. Returns True if myString is a Palindrome
+		public static bool PalindromeCheck(string myString)
+		{
+			myString = myString.ToLower();
+			char[] myArray = myString.ToCharArray();
+			Array.Reverse(myArray);
+			string revString = new string(myArray);
+			return myString == revString;
+		}
+
+		// 5. Returns array of all odd numbers in allNums array
+		public static int[] OddArray(int[] allNums)
+		{
+			int count = 0;
+			//Counts number of odds in the allNums array
+			for (int i = 0; i < allNums.Length; i++)
+			{
+				if ((allNums[i] % 2) == 1)
+				{
+					count++;
+				}
+			}
+			//creates new array with the length of count
+			int[] oddNums = new int[count];
+
+			//resets count to be OddNums index
+			count = 0;
+			//Fills oddNums with all the odd numbers in allNums
+			for (int i = 0; i < allNums.Length; i++)
+			{
+				if ((allNums[i] % 2) == 1)
+				{
+					oddNums[count] = allNums[i];
+					count++;
+				}
+			}
+			return oddNums;
+		}
+
+		// 6. Multiplies all the numbers of a List
+		public static int Multiply(List<int> numList)
+		{
+			int product = numList[0];
+			for (int i = 1; i < numList.Count; i++)
+			{
+				product = product * numList[i];
+			}
+			return product;
+		}
+
+		// 7. Checks if a list is sorted
+		public static bool IsSorted(List<double> numList)
+		{
+			//sets testSort to true
+			bool testSort = true;
+			double check = numList[0];
+			for (int i = 1; i < numList.Count; i++)
+			{
+				if (check > numList[i])
+				{
+					testSort = false;
+					break;
+				}
+				else
+				{
+					check = numList[i];
+				}
+			}
+			return testSort;
+		}
+
+		// 8. Returns the smallest element of an Array
+		public static int Lowest(int[] testArray)
+		{
+			int low = testArray[0];
+			for (int i = 1; i < testArray.Length; i++)
+			{
+				if (testArray[i] < low)
+				{
+					low = testArray[i];
+				}
+			}
+			return low;
+		}
+
+
+		//left off here 9/6/16
+		// 9. Returns the number of duplicates in an list
+		public static int Dups(List<char> charList)
+		{
+			List<char> duplicates = new List<char>();
+			List<char> unique = new List<char>();
+			foreach (char i in charList)
+			{
+				if (!unique.Contains(i))
+				{
+					unique.Add(i);
+				}
+				else {
+					duplicates.Add(i);
+				}
+			}
+			return duplicates.Count;
+		}
+
+
+		// 10. returns the lowest value of a dictionary of string(key) and integer(value)
+		public static int LowestValue(Dictionary<string, int> allValues)
+		{
+			var testNums = allValues.GetEnumerator();
+			testNums.MoveNext();
+			int low = testNums.Current.Value;
+			foreach (KeyValuePair<string, int> i in allValues)
+			{
+				if (i.Value < low)
+				{
+					low = i.Value;
+				}
+			}
+			return low;
+		}
+
+		// 11. checks if a sentence is a pangram
+		public static bool PanGramCheck(string sentence)
+		{
+			if (sentence.Length < 26)
+			{
+				return false;
+			}
+			sentence = sentence.ToLower();
+			//char[] newSen = sentence.ToCharArray();
+			//char[] alphabet2 = new char[26]{'a','b','c','d','e','f','g','h','i', 'j','k','l', 'm', 'n',
+			//	'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+			int count = 0;
+			bool[] alphabet = new bool[26];
+
+			foreach (char i in sentence)
+			{
+				if (i >= 'a' && i <= 'z')
+				{
+					//index is the current value of i -97 which is the ascii value for 'a'
+					int index = i - 97;
+					if (!alphabet[index])
+					{
+						alphabet[index] = true;
+						count++;
+					}
+				}
+
+			}
+			return count == 26;
+		}
+
+		// 12. Checks is a two-dimentional array is Lo Shu Magic Square
+		public static bool LoShu(int[,] magic)
+		{
+			int sum = 0;
+			for (int i = 0; i < magic.GetLength(0); i++)
+			{
+				sum += magic[i, 0];
+				for (int j = 0; j < magic.GetLength(1); j++)
+				{
+					Console.Write(magic[i, j] + "\t");
+				}
+				Console.WriteLine();
+			}
+			//List<bool> tests = new List<bool>();
+			if (magic.GetLength(0) != magic.GetLength(1))
+			{ return false; }
+			else if (!numCheck(magic))
+			{ return false; }
+			else if (!checkSum(magic, 0, sum))
+			{ return false; }
+			else if (!checkSum(magic, 1, sum))
+			{ return false; }
+			else if (!diaCheckSum1(magic))
+			{ return false; }
+			else if (!diaCheckSum2(magic))
+			{ return false; }
+
+			return true;
+		}
+		//Aux methods for Loshu method
+		public static bool numCheck(int[,] magic)
+		{
+			bool[] checkNum = new bool[9];
+			int count = 0;
+			foreach (int s in magic)
+			{
+				if (!checkNum[s - 1])
+				{
+					checkNum[s - 1] = true;
+					count++;
+				}
+			}
+			return count == magic.Length;
+		}
+
+		public static bool checkSum(int[,] magic, int sw, int sum)
+		{
+			//*note: sw is a switch variable to 0 to add rows, 1 to add cols
+			int sumTest;
+			for (int i = 0; i < magic.GetLength(0); i++)
+			{
+				sumTest = 0;
+				for (int j = 0; j < magic.GetLength(1); j++)
+				{
+					if (sw == 0)
+					{ sumTest += magic[i, j]; }
+					if (sw == 1)
+					{ sumTest += magic[j, i]; }
+				}
+				if (sum != sumTest)
+				{ return false; }
+			}
+			return true;
+		}
+
+		public static bool diaCheckSum1(int[,] magic)
+		{
+			int sum = 15;
+			int sumTest = 0;
+			for (int i = 0; i < magic.GetLength(0); i++)
+			{
+				sumTest += magic[i, i];
+			}
+			return sum == sumTest;
+		}
+
+		public static bool diaCheckSum2(int[,] magic)
+		{
+			int sum = 15;
+			int sumTest = 0;
+			int temp = 2;
+			for (int i = 0; i < magic.GetLength(0); i++)
+			{
+				sumTest += magic[i, temp--];
+			}
+			return sum == sumTest;
+		}
+
+		// 13. Takes a list of words and an integer n and returns the
+		// list words whose length is greater han n
+		public static List<string> FilterLongWords(List<string> wordList, int n)
+		{
+			List<string> myWords = new List<string>();
+			foreach (string s in wordList)
+			{
+				if (s.Length > n)
+				{
+					myWords.Add(s.ToUpper());
+				}
+			}
+			return myWords;
+		}
+
+		// 14. Takes a dictionary of names and scores and returns a dictionary of the 
+		// top ten items sorted by value
+		public static Dictionary<string, int> TopTen(Dictionary<string, int> allScores)
+		{
+			Dictionary<string, int> firstTen = new Dictionary<string, int>();
+			int count = 0;
+
+			foreach (KeyValuePair<string, int> kvp in allScores.OrderByDescending(key => key.Value))
+			{
+				Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+				firstTen.Add(kvp.Key, kvp.Value);
+				count++;
+				if (count == 10)
+				{
+					break;
+				}
+			}
+			return firstTen;
+		}
+
+		// Main method to test the homework
+		public static void Main()
+		{
+			//1 test for TwoMax
+			Console.WriteLine("Which is larger: 4, 2 ");
+			Console.WriteLine(TwoMax(4, 2));
+
+			//2 test for ThreeMax
+			Console.WriteLine("Which is larger: 4, 9 ,4");
+			Console.WriteLine(ThreeMax(4, 9, 4));
+			Console.WriteLine("Press Enter to Continue");
+			Console.ReadLine();
+
+			//3 test for starts with vowel
+			Console.WriteLine("Is it a vowel: E");
+			Console.WriteLine(StartsWithVowel('E'));
+			Console.WriteLine("Is it a vowel: V");
+			Console.WriteLine(StartsWithVowel('V'));
+			Console.WriteLine("Press Enter to Continue");
+			Console.ReadLine();
+
+			//4 test for Palindrome
+			Console.WriteLine("Is it a Palindrome: abcfcbA");
+			Console.WriteLine(PalindromeCheck("abcfcbA"));
+			Console.WriteLine("Is it a Palindrome: abbfa");
+			Console.WriteLine(PalindromeCheck("abbfa"));
+			Console.WriteLine("Press Enter to Continue");
+			Console.ReadLine();
+
+
+			//5 Odds in Array
+			int[] testArray = { 1, 2, 1, 2, 1, 2, 3, 3 };
+			Console.WriteLine("How many odd in the array: "); //, testArray);
+			foreach (int i in testArray)
+			{
+				Console.Write(i + " ");
+			}
+			Console.WriteLine("\nThe odd numbers of the array are: ");
+			foreach (int i in OddArray(testArray))
+			{
+				Console.Write(i + " ");
+			}
+			Console.WriteLine("\nPress Enter to Continue");
+			Console.ReadLine();
+
+			//6 multiplies the values of a integer list
+			List<int> myList = new List<int>();
+			myList.Add(2);
+			myList.Add(2);
+			myList.Add(5);
+			Console.WriteLine("\nThe list has he following");
+			foreach (int i in myList)
+			{
+				Console.Write(i + " ");
+			}
+			Console.WriteLine("\nTheir product is {0}", Multiply(myList));
+			Console.WriteLine("Press Enter to Continue");
+			Console.ReadLine();
+
+			// 7. Checks if a list of doubles is sorted
+			List<double> doubleList = new List<double>();
+			doubleList.Add(2.1);
+			doubleList.Add(4.3);
+			doubleList.Add(5.2);
+			doubleList.Add(6.9);
+			Console.WriteLine("\nThe list has the following: ");
+			foreach (double i in doubleList)
+			{
+				Console.Write(i + " ");
+			}
+			Console.WriteLine("\nIs this list sorted: {0}", IsSorted(doubleList));
+			Console.WriteLine("Press Enter to Continue");
+			Console.ReadLine();
+
+			// 8. Returns the smallest element of an Array
+			testArray = new int[8] { 6, 3, 7, 9, 2, 1, 8, 4 };
+			Console.WriteLine("The array has the following");
+			foreach (int i in testArray)
+			{
+				Console.Write(i + " ");
+			}
+			Console.WriteLine("\n{0} is the lowest.", Lowest(testArray));
+			Console.WriteLine("Press Enter to Continue");
+			Console.ReadLine();
+
+			// 9. Returns the number of duplicates in an list
+			List<char> charList = new List<char>();
+			charList.Add('n');
+			charList.Add('a');
+			charList.Add('w');
+			charList.Add('a');
+			charList.Add('d');
+			charList.Add('e');
+			charList.Add('w');
+			Console.WriteLine("\nThe list has he following");
+			foreach (char i in charList)
+			{
+				Console.Write(i + " ");
+			}
+			Console.WriteLine("\nThe array has {0} duplicates", Dups(charList));
+			Console.WriteLine("Press Enter to Continue");
+			Console.ReadLine();
+
+			// 10. Lowest value in an dictionary
+			Dictionary<string, int> testD = new Dictionary<string, int>();
+			testD.Add("Apples", 3);
+			testD.Add("Pears", 7);
+			testD.Add("Pineapples", 2);
+			testD.Add("Kiwis", 4);
+			testD.Add("Peaches", 1);
+			Console.WriteLine("From the following Key Value Pair:");
+			foreach (KeyValuePair<string, int> kpv in testD)
+			{
+				Console.WriteLine("Key: {0}, Value: {1}", kpv.Key, kpv.Value);
+			}
+			Console.WriteLine("The lowest value is {0}", LowestValue(testD));
+			Console.WriteLine("Press Enter to Continue");
+			Console.ReadLine();
+
+			// 11. Pangram check
+			string pgString = "The quick brown fox jumps over the lazy dog";
+			Console.WriteLine("Is \"{0}\" a Pangram : {1}", pgString, PanGramCheck(pgString));
+			Console.WriteLine("Press Enter to Continue");
+			Console.ReadLine();
+
+			// 12. LoShu
+			int[,] loShuArray = { { 4, 9, 2 }, { 3, 5, 7 }, { 8, 1, 6 } };
+			Console.WriteLine("Is the Above a LoShu Square: {0}", LoShu(loShuArray));
+			Console.WriteLine("Press Enter to Continue");
+			Console.ReadLine();
+
+			// 13. List of Words larger than n
+			List<string> words = new List<string>();
+			words.Add("Television");
+			words.Add("Radio");
+			words.Add("Computer");
+			words.Add("Phone");
+			Console.WriteLine("Current word list");
+			foreach (string s in words)
+			{
+				Console.WriteLine(s);
+			}
+			Console.WriteLine("\nWords Larger than 6");
+			foreach (string s in FilterLongWords(words, 6))
+			{
+				Console.WriteLine(s);
+			}
+			Console.WriteLine("Press Enter to Continue");
+			Console.ReadLine();
+
+			// 14. Top Ten
+			testD.Add("Bananas", 10);
+			testD.Add("Mangos", 3);
+			testD.Add("Papaya", 6);
+			testD.Add("Grapes", 4);
+			testD.Add("Oranges", 7);
+			testD.Add("Uniq", 8);
+			Console.WriteLine("Current Dictionary of Words");
+			foreach (KeyValuePair<string, int> w in testD)
+			{
+				Console.WriteLine("Key: {0}, Value {1}", w.Key, w.Value);
+			}
+			Console.WriteLine("\nTop Ten Dictionary of Words");
+			TopTen(testD);
+			/*
+			foreach (KeyValuePair<string, int> w in )
+			{
+				Console.WriteLine("Key: {0}, Value {1}", w.Key, w.Value);
+			}*/
+			Console.WriteLine("Press Enter to Continue");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ HW1Test.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace HW1Test
+{
+	[TestFixture]
+	public class HW1Test
+	{
+		//loShu square arrays
+		int[,] loShu = { { 8, 3, 4 }, { 1, 5, 9 }, { 6, 7, 2 } };
+		int[,] shu = { { 1, 5, 3 }, { 7, 4, 9 }, { 2, 6, 8 } };
+		int[,] doShu = { { 1, 4, 3 }, { 7, 4, 9 }, { 2, 3, 7 } };
+		int[,] shoShu = { { 1, 5 }, { 7, 4 }, { 2, 6 } };
+
+		[Test]//1
+		public void TwoMax_TestCase_Pass()
+		{
+			Assert.AreEqual(9, HW1Code.TwoMax(3, 9));
+			Assert.AreEqual(7, HW1Code.TwoMax(7, 2));
+			Assert.AreEqual(3, HW1Code.TwoMax(3, 3));
+		}
+
+		[Test]//2
+		public void ThreeMax_TestCase_Pass()
+		{
+			Assert.AreEqual(9, HW1Code.ThreeMax(2, 9, 5));
+			Assert.AreEqual(5, HW1Code.ThreeMax(5, 5, 5));
+			Assert.AreEqual(8, HW1Code.ThreeMax(1, 2, 8));
+			Assert.AreEqual(7, HW1Code.ThreeMax(1, 7, 7));
+			Assert.AreEqual(4, HW1Code.ThreeMax(4, 4, 2));
+		}
+
+		[Test]//3
+		public void StartsWithVowel_TestCase_Pass()
+		{
+			Assert.True(HW1Code.StartsWithVowel('e'));
+			Assert.True(HW1Code.StartsWithVowel('U'));
+			Assert.False(HW1Code.StartsWithVowel('f'));
+			Assert.False(HW1Code.StartsWithVowel('R'));
+		}
+
+		[Test]//4
+		public void PalindromeCheck_TestCase_Pass()
+		{
+			Assert.True(HW1Code.PalindromeCheck("AbbA"));
+			Assert.True(HW1Code.PalindromeCheck("AbcbA"));
+			Assert.False(HW1Code.PalindromeCheck("abcd"));
+			Assert.False(HW1Code.PalindromeCheck("abcD"));
+		}
+
+		[Test]//5
+		public void OddArray_TestCase_Pass()
+		{
+			int[] source = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+			int[] expResult = { 1, 3, 5, 7, 9 };
+
+			Assert.AreEqual(expResult, HW1Code.OddArray(source));
+			Assert.AreEqual(expResult.Length, HW1Code.OddArray(source).Length);
+
+			source = new int[] { 2, 4, 6, 8 };
+			Assert.IsEmpty(HW1Code.OddArray(source));
+		}
+
+		[Test]//6
+		public void Multiply_TestCase()
+		{
+			List<int> myList = new List<int>();
+			myList.Add(2);
+			myList.Add(2);
+			myList.Add(5);
+			Assert.AreEqual(20, HW1Code.Multiply(myList));
+			myList.Add(4);
+			Assert.AreEqual(80, HW1Code.Multiply(myList));
+			myList.Insert(2, 0);
+			Assert.AreEqual(0, HW1Code.Multiply(myList));
+		}
+
+		[Test]//7
+		public void IsSorted_TestCase()
+		{
+			List<double> doubleList = new List<double>();
+			doubleList.Add(1.5);
+			doubleList.Add(3.7);
+			doubleList.Add(6.4);
+			doubleList.Add(8.0);
+			Assert.IsTrue(HW1Code.IsSorted(doubleList));
+			doubleList.Add(2.1);
+			Assert.IsFalse(HW1Code.IsSorted(doubleList));
+		}
+
+		[Test]//8
+		public void Lowest_TestCase()
+		{
+			int[] temp = { 5, 2, 1, 7, 8 };
+			Assert.AreEqual(1, HW1Code.Lowest(temp));
+		}
+
+		[Test]//9 duplicates
+		public void Dups_TestCase()
+		{
+			List<char> charList = new List<char>();
+			charList.Add('s');
+			charList.Add('t');
+			charList.Add('q');
+			charList.Add('n');
+			Assert.AreEqual(0, HW1Code.Dups(charList));
+			charList.Add('s');
+			Assert.AreEqual(1, HW1Code.Dups(charList));
+			charList.Add('s');
+			Assert.AreEqual(2, HW1Code.Dups(charList));
+
+		}
+
+		[Test]//10 Smallest in Dictionary
+		public void LowestValue_TestCase()
+		{
+			Dictionary<string, int> testValue = new Dictionary<string, int>();
+			testValue.Add("Apples", 5);
+			testValue.Add("Grapes", 6);
+			testValue.Add("Peaches", 3);
+			testValue.Add("Melons", 2);
+			Assert.AreEqual(2, HW1Code.LowestValue(testValue));
+			testValue.Add("Oranges", 5);
+			Assert.AreEqual(2, HW1Code.LowestValue(testValue));
+		}
+
+		[Test]//11 Pangram test
+		public void PanGrmCheck_TestCase()
+		{
+			string panTrue = "The five boxing wizards jump quickly";
+			string panFalse = "Any other Sentence will work for this method";
+			Assert.IsTrue(HW1Code.PanGramCheck(panTrue));
+			Assert.IsFalse(HW1Code.PanGramCheck(panFalse));
+			Assert.IsFalse(HW1Code.PanGramCheck("Too Short"));
+		}
+
+		[Test]//12 LoShu Square Checker
+		public void LoShuChecker_TestCase()
+		{
+			Assert.IsTrue(HW1Code.LoShu(loShu));
+			Assert.IsFalse(HW1Code.LoShu(shu));
+			Assert.IsFalse(HW1Code.LoShu(doShu));
+			Assert.IsFalse(HW1Code.LoShu(shoShu));
+		}
+
+		[Test]
+		public void numCheckTest()
+		{
+
+			Assert.IsTrue(HW1Code.numCheck(loShu));
+			Assert.IsTrue(HW1Code.numCheck(shu));
+			Assert.IsFalse(HW1Code.numCheck(doShu));
+			//no longer needed because if (magic.GetLength(0) != magic.GetLength(1))
+			//takes care of this flaw
+			//Assert.IsFalse(HW1Code.numCheck(shoShu));
+		}
+
+		[Test]
+		public void checkSumTest0()
+		{
+			Assert.IsTrue(HW1Code.checkSum(loShu, 0, 15));
+			Assert.IsFalse(HW1Code.checkSum(shu, 0, 15));
+			Assert.IsFalse(HW1Code.checkSum(doShu, 0, 15));
+			Assert.IsFalse(HW1Code.checkSum(shoShu, 0, 15));
+		}
+
+		[Test]
+		public void checkSumTest1()
+		{
+			Assert.IsTrue(HW1Code.checkSum(loShu, 1, 15));
+			Assert.IsFalse(HW1Code.checkSum(shu, 1, 15));
+			Assert.IsFalse(HW1Code.checkSum(doShu, 1, 15));
+			Assert.IsFalse(HW1Code.checkSum(shoShu, 1, 15));
+		}
+
+		[Test]
+		public void DiagTest1()
+		{
+			Assert.IsTrue(HW1Code.diaCheckSum1(loShu));
+			Assert.IsFalse(HW1Code.diaCheckSum1(shu));
+			Assert.IsFalse(HW1Code.diaCheckSum1(doShu));
+			//Assert.IsFalse(HW1Code.diaCheckSum1(shoShu));
+		}
+
+		[Test]
+		public void DiagTest2()
+		{
+			Assert.IsTrue(HW1Code.diaCheckSum2(loShu));
+			Assert.IsFalse(HW1Code.diaCheckSum2(shu));
+			Assert.IsFalse(HW1Code.diaCheckSum2(doShu));
+			//Assert.IsFalse(HW1Code.diaCheckSum2(shoShu));
+
+		}
+
+		[Test]//13 wordList
+		public void FilterLongWords_TestCase()
+		{
+			List<string> testWords = new List<string>();
+			testWords.Add("Cows");
+			testWords.Add("Tigers");
+			testWords.Add("Elephants");
+			testWords.Add("Lions");
+			testWords.Add("Bears");
+			testWords.Add("Octopuses");
+
+			List<string> newWords = new List<string>();
+			newWords.Add("ELEPHANTS");
+			newWords.Add("OCTOPUSES");
+
+			Assert.AreEqual(newWords, HW1Code.FilterLongWords(testWords, 7));
+		}
+
+		[Test]//14 Topten
+		public void TopTen_TestCase()
+		{
+			//The Setup
+			Dictionary<string, int> sendDic = new Dictionary<string, int>();
+			sendDic.Add("Tom", 300);
+			sendDic.Add("Jim", 3000);
+			sendDic.Add("James", 236);
+			sendDic.Add("Jorge", 1562);
+			sendDic.Add("Hazel", 19553);
+			sendDic.Add("Maria", 18523);
+			sendDic.Add("Albert", 8731);
+			sendDic.Add("Ed", 2498);
+			sendDic.Add("Lucy", 5483);
+			sendDic.Add("Mike", 6781);
+			sendDic.Add("Arnold", 1568);
+			sendDic.Add("Jessy", 1867);
+
+			Dictionary<string, int> actualDic = new Dictionary<string, int>();
+			actualDic.Add("Hazel", 19553);
+			actualDic.Add("Maria", 18523);
+			actualDic.Add("Albert", 8731);
+			actualDic.Add("Mike", 6781);
+			actualDic.Add("Lucy", 5483);
+			actualDic.Add("Jim", 3000);
+			actualDic.Add("Ed", 2498);
+			actualDic.Add("Jessy", 1867);
+			actualDic.Add("Arnold", 1568);
+			actualDic.Add("Jorge", 1562);
+
+			Assert.IsInstanceOf<Dictionary<string, int>>(HW1Code.TopTen(sendDic));
+			Assert.AreEqual(actualDic, HW1Code.TopTen(sendDic));
+		}
+	}
+}
